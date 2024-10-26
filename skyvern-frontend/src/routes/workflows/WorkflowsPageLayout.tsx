25ac35c9e6dafdbc67f9e71d04b6a8cf88d0afb7
Ka220 <guang588888@icloud.com>
2024-10-27 04:34:03 +0700

    更新 WorkflowsPageLayout.tsx

--- skyvern-frontend/src/routes/workflows/WorkflowsPageLayout.tsx
+++ skyvern-frontend/src/routes/workflows/WorkflowsPageLayout.tsx
@@ -1,18 +1,1 @@
-import { cn } from "@/util/utils";
-import { Outlet, useMatch } from "react-router-dom";
 
-function WorkflowsPageLayout() {
-  const match = useMatch("/workflows/:workflowPermanentId/edit");
-
-  return (
-    <main
-      className={cn({
-        "container mx-auto px-8": !match,
-      })}
-    >
-      <Outlet />
-    </main>
-  );
-}
-
-export { WorkflowsPageLayout };
