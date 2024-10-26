0cb4d2da0ec90c4e0dfac94ddc2391d725f9face
Ka220 <guang588888@icloud.com>
2024-10-27 04:26:05 +0700

    更新 useMountEffect.ts

--- skyvern-frontend/src/hooks/useMountEffect.ts
+++ skyvern-frontend/src/hooks/useMountEffect.ts
@@ -1,10 +1,1 @@
-import { useEffect } from "react";
 
-function useMountEffect(callback: () => void) {
-  return useEffect(() => {
-    callback();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-}
-
-export { useMountEffect };
