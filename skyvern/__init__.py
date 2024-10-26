248604e5f0d3c57b6b5ebab6a72da015e3f45ac3
Ka220 <guang588888@icloud.com>
2024-10-27 02:44:21 +0700

    更新 __init__.py

--- skyvern/__init__.py
+++ skyvern/__init__.py
@@ -1,13 +1,1 @@
-from ddtrace import tracer
-from ddtrace.filters import FilterRequestsOnUrl
 
-from skyvern.forge.sdk.forge_log import setup_logger
-
-tracer.configure(
-    settings={
-        "FILTERS": [
-            FilterRequestsOnUrl(r"http://.*/heartbeat$"),
-        ],
-    },
-)
-setup_logger()
