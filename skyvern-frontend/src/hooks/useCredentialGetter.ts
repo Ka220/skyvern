327479f06638d2e54cdccfe6ab27c27c83c029db
Ka220 <guang588888@icloud.com>
2024-10-27 04:25:48 +0700

    更新 useCredentialGetter.ts

--- skyvern-frontend/src/hooks/useCredentialGetter.ts
+++ skyvern-frontend/src/hooks/useCredentialGetter.ts
@@ -1,9 +1,1 @@
-import { CredentialGetterContext } from "@/store/CredentialGetterContext";
-import { useContext } from "react";
 
-function useCredentialGetter() {
-  const credentialGetter = useContext(CredentialGetterContext);
-  return credentialGetter;
-}
-
-export { useCredentialGetter };
