db95a519f1fca5e31477098f9c2d4f91869ea431
Ka220 <guang588888@icloud.com>
2024-10-27 03:47:13 +0700

    更新 organizations.py

--- skyvern/forge/sdk/schemas/organizations.py
+++ skyvern/forge/sdk/schemas/organizations.py
@@ -1,15 +1,1 @@
-from pydantic import BaseModel
 
-from skyvern.forge.sdk.models import Organization, OrganizationAuthToken
-
-
-class GetOrganizationsResponse(BaseModel):
-    organizations: list[Organization]
-
-
-class GetOrganizationAPIKeysResponse(BaseModel):
-    api_keys: list[OrganizationAuthToken]
-
-
-class OrganizationUpdate(BaseModel):
-    max_steps_per_run: int | None = None
